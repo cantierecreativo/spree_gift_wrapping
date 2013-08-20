6903f49a4f76e2835d6747a4bb2b99e29f32a7e3
David Librera <davidlibrear@gmail.com>
2013-08-20 15:33:30 +0200

    fixed layout, removed js and added additional checkout step

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,11 +1,17 @@
-Spree::Order.class_eval do
+module Spree
+  Order.class_eval do
+    preference :gift_wrapping, :boolean, default: false
+    preference :gift_wrapping_receiving_email, :text
 
-  preference :gift_wrapping, :boolean, default: false
-  preference :gift_wrapping_receiving_email, :text
+    preference :gift_wrapping_intestation, :text
+    preference :gift_wrapping_message, :text
+    preference :gift_wrapping_sign, :text
 
-  preference :gift_wrapping_intestation, :text
-  preference :gift_wrapping_message, :text
-  preference :gift_wrapping_sign, :text
+    attr_accessible :preferred_gift_wrapping, :preferred_gift_wrapping_message, :preferred_gift_wrapping_intestation, :preferred_gift_wrapping_sign, :preferred_gift_wrapping_receiving_email
 
-  attr_accessible :preferred_gift_wrapping, :preferred_gift_wrapping_message, :preferred_gift_wrapping_intestation, :preferred_gift_wrapping_sign, :preferred_gift_wrapping_receiving_email
+#    insert_checkout_step :gift, before: :address, if: lambda { |order| order.prefers_gift_wrapping? }
+    [:preferred_gift_wrapping_intestation, :preferred_gift_wrapping_message, :preferred_gift_wrapping_sign].each do |attrib|
+      validates attrib, presence: true, if: lambda { self.prefers_gift_wrapping? && self.state == "gift" }
+    end
+  end
 end
